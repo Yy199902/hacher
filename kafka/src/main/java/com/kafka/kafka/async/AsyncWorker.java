a2be409b66d1109a1510959bb233fb7d27363d64
yuyin <yuyin3306>
2021-01-03 14:47:21 +0800

    自定义线程池

--- kafka/src/main/java/com/kafka/kafka/async/AsyncWorker.java
+++ kafka/src/main/java/com/kafka/kafka/async/AsyncWorker.java
@@ -2,6 +2,9 @@ package com.kafka.kafka.async;
 
 import java.util.concurrent.*;
 
+/**
+ * @author 86158
+ */
 public class AsyncWorker {
 
     private AsyncWorker(){
@@ -9,23 +12,13 @@ public class AsyncWorker {
     }
     //当线程空闲30s开启回收
     private static final int KEEP_ALIVE_TIME = 30;
-    private static final ThreadPoolExecutor threadPool =new ThreadPoolExecutor(5, Integer.MAX_VALUE, KEEP_ALIVE_TIME, TimeUnit.SECONDS,
-            new SynchronousQueue<Runnable>(), new RejectedExecutionHandler() {
-        @Override
-        public void rejectedExecution(Runnable r, ThreadPoolExecutor e) {
-            throw new RejectedExecutionException(String.format(
-                    "Thread pool is EXHAUSTED! Thread Name: %s, Pool Size: %d (active: %d, core: %d, max: %d, largest: %d), " +
-                            "Queue: %d, Task: %d (completed: %d), Executor status:(isShutdown:%s, isTerminated:%s, isTerminating:%s)",
-                    Thread.currentThread().getName(), e.getPoolSize(), e.getActiveCount(), e.getCorePoolSize(),
-                    e.getMaximumPoolSize(), e.getLargestPoolSize(), e.getQueue().size(), e.getTaskCount(), e.getCompletedTaskCount(),
-                    e.isShutdown(), e.isTerminated(), e.isTerminating()));
-        }
-    });
+    private static final ThreadPoolExecutor THREAD_POOL =new ThreadPoolExecutor(5, Integer.MAX_VALUE, KEEP_ALIVE_TIME, TimeUnit.SECONDS,
+            new SynchronousQueue<Runnable>(), new DefaultThreadFactory("测试"));
     public static void execute(final Runnable runnable) {
-        threadPool.execute(runnable);
+        THREAD_POOL.execute(runnable);
     }
 
     public static void shutdown() {
-        threadPool.shutdown();
+        THREAD_POOL.shutdown();
     }
 }
